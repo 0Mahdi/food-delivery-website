9d0d5c239379d67642d202f0a6eabb30731203fd
Mahdi Hossen <114096228+0Mahdi@users.noreply.github.com>
2024-05-18 19:03:12 +0600

    create place order feature and stripe payment integration

--- backend/server.js
+++ backend/server.js
@@ -1,33 +1,34 @@
-import express from  "express"
-import cors from "cors"
-import { connectDB } from "./config/db.js"
-import foodRouter from "./routes/foodRoute.js"
-import userRouter from "./routes/userRoute.js"
-import 'dotenv/config.js'
-import cartRouter from "./routes/cartRoute.js"
+import express from "express";
+import cors from "cors";
+import { connectDB } from "./config/db.js";
+import foodRouter from "./routes/foodRoute.js";
+import userRouter from "./routes/userRoute.js";
+import 'dotenv/config.js';
+import cartRouter from "./routes/cartRoute.js";
+import orderRouter from "./routes/orderRoute.js";
 
+// App config
+const app = express();
+const port = 4000;
 
-//app config
-const app = express()
-const port = 4000
+// Middleware
+app.use(express.json());
+app.use(cors());
 
-//middleware
-app.use(express.json())
-app.use(cors())
-
-// db connection
+// DB connection
 connectDB();
 
-//api endpoints
-app.use("/api/food", foodRouter)
-app.use("/images",express.static('uploads'))
-app.use("/api/user",userRouter)
-app.use("/api/cart",cartRouter)
+// API endpoints
+app.use("/api/food", foodRouter);
+app.use("/images", express.static('uploads'));
+app.use("/api/user", userRouter);
+app.use("/api/cart", cartRouter);
+app.use("/api/order", orderRouter);
 
-app.get("/",(req, res)=>{
-    res.send("API Working")
-})
+app.get("/", (req, res) => {
+    res.send("API Working");
+});
 
-app.listen(port,()=>{
-    console.log(`Server Started on http://localhost:${port}`)
-})
+app.listen(port, () => {
+    console.log(`Server Started on http://localhost:${port}`);
+});
